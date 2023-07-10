f45ceaf7d1b3671e41b3b4be48ad9e22240c7c55
Rujjal Sada <rujjalsada@Rujjals-MacBook-Air.local>
2023-07-10 22:56:21 +0200

    memory added

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from PyPDF2 import  PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
-import warnings
-from urllib3.exceptions import InsecureRequestWarning
+# import os
 
-warnings.filterwarnings("ignore", category=InsecureRequestWarning)
-
+# os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 def get_pdf_text(pdf_docs):
@@ -31,14 +32,27 @@ def get_text_chunks(text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceInstructEmbeddings(model_name = "bert-base-uncased")
+    embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
+
+def get_conservation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key = "Chat-history", return_messages= True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory= memory
+    )
+    return conversation_chain
 
 
 def main():
     load_dotenv()
     st.set_page_config("Chatbot for married people", page_icon=":books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.header("Chatbot for married people :books:")
     st.text_input("Ask a question ")
@@ -58,6 +72,22 @@ def main():
                 #create vector store
                 vectorstore = get_vectorstore(text_chunks)
                 #vectorstore.save("vectorstore.faiss")
+                #create conservation chain
+                st.session_state.conversation = get_conservation_chain(vectorstore)
+
 
 if __name__ == '__main__':
+    # import os
+
+    # # Clear the cached model files
+    # cached_model_dir = "/Users/rujjalsada/.cache/torch/sentence_transformers/"
+    # if os.path.exists(cached_model_dir):
+    #     os.system(f"rm -rf {cached_model_dir}")
+
+    # # Upgrade sentence_transformers
+    # os.system("pip install --upgrade sentence-transformers")
+
+    # # Set TOKENIZERS_PARALLELISM environment variable
+    # os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
     main()
